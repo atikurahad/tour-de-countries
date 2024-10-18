dfa58d25f702cbbb05b0f050f8dbd7ec25fcc400
unknown <atikurahad@gmail.com>
2024-10-18 22:57:45 +0600

    mod components

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,11 +1,14 @@
 const Country = ({ country }) => {
-  const { name, capital, flags } = country;
+  const { name, capital, flags,population } = country;
   return (
     <div>
       <img src={flags.png} alt="img" className="h-48 w-full" />
 
-      <h4>Name: {name?.common}</h4>
-      <h4>Capital: {capital}</h4>
+     <div className="flex flex-col items-center text-white text-xl font-bold">
+     <h4>Name: {name?.common}</h4>
+      <h4 >Capital: <span className="text-black">{capital}</span> </h4>
+      <h2>Population: {population}</h2>
+     </div>
     </div>
   );
 };
